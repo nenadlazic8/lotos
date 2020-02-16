0f73f178e8a6f78d7582e4f95bdb496812e6c9e4
nenadlazic8 <45943346+nenadlazic8@users.noreply.github.com>
2020-02-16 12:36:12 +0100

    Add files via upload

--- lotos/public_html/js/main.js
+++ lotos/public_html/js/main.js
@@ -9,7 +9,34 @@ $(document).ready(function () {
 
     $('.count-num').rCounter();
 
+    $(window).on('scroll', function(){
+        if($(window).scrollTop()){
+            $('.navigation').addClass('position-fixed w-100 bg-dark').css({
+                'z-index' : '3', 
+                'padding' : '0px',
+                'transition' : '.5s linear'
+            });
+            $('.navbar-brand img').css({
+                'max-width' : '65%',
+                'transition' : '.5s linear'
+            });
+        }
+        else{
+            $('.navigation').removeClass('position-fixed w-100 bg-dark').css({
+                'z-index' : '3', 
+                'padding' : '25px 35px',
+                'transition' : '.5s linear'
+            });
+            $('.navbar-brand img').css({
+                'max-width' : '100%'
+            });
+        }
+    });
 
+
+    $('.nav-link').click(function(){
+        $('.navbar-collapse').removeClass('show').addClass('hide');
+    });
 
 
 });
